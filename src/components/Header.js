08e4f009002728a831801ab737b7ecc359e1b703
McCambley <jakemccamb16@gmail.com>
2021-09-13 16:27:24 -0400

    Dev: complete header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 import logo from '../images/logo.svg';
+import hamburgerOpen from '../images/hamburgerOpen.svg';
+import hamburgerClose from '../images/hamburgerClose.svg';
 import { Link } from 'react-router-dom';
+import React from 'react';
 
 export default function Header({ loggedIn, navText, handleNavClick, path }) {
+  const [isHamburgerOpen, setIsHamburgerOpen] = React.useState(false);
   const email = 'Jakemccamb16@gmail.com';
+
+  function handleHamburgerClick() {
+    setIsHamburgerOpen(!isHamburgerOpen);
+  }
   return (
     <>
       {/* fancy dropdown menu */}
       {loggedIn && (
-        <div className="header__dropdown header__dropdown_open">
+        <div className={`header__dropdown ${isHamburgerOpen && 'header__dropdown_open'}`}>
           <p className="header__dropdown-email">{email}</p>
           <Link to={path} className="header__dropdown-link">
             {navText}
@@ -20,16 +28,24 @@ export default function Header({ loggedIn, navText, handleNavClick, path }) {
           <img id="logo" src={logo} alt="Around the U.S." className="logo" />
         </Link>
         {loggedIn ? (
-          <nav className="header__nav-container">
-            <p className="header__email">{email}</p>
-            <Link to={path} className="header__link">
-              {navText}
-            </Link>
-            <button className="header__hamburger" />
-          </nav>
+          <div>
+            <nav className="header__nav-container">
+              <p className="header__email">{email}</p>
+              <Link to={path} className="header__link">
+                {navText}
+              </Link>
+            </nav>
+            <button className="header__hamburger" onClick={handleHamburgerClick}>
+              <img
+                src={!isHamburgerOpen ? hamburgerOpen : hamburgerClose}
+                className="header__hamburger-icon"
+                alt=""
+              />
+            </button>
+          </div>
         ) : (
           <nav className="header__nav-container">
-            <Link to={path} className="header__link">
+            <Link to={path} className="header__link header__link_loggedout">
               {navText}
             </Link>
           </nav>
