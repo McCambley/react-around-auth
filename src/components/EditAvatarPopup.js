b0f23ccab63f1d9f145381211ed0db5f9df9ff58
McCambley <jakemccamb16@gmail.com>
2021-09-17 22:41:10 -0400

    Fix: control EditAvatarPopup

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,49 +1,60 @@
 import PopupWithForm from './PopupWithForm';
 import React from 'react';
 
-export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar, isSubmitting, checkValidity }) {
-  const inputRef = React.useRef();
+export default function EditAvatarPopup({
+  isOpen,
+  onClose,
+  onUpdateAvatar,
+  isSubmitting,
+  checkValidity,
+}) {
   const [isUrlInputValid, updateUrlInputValidity] = React.useState(true);
   const [errorMessage, updateErrorMessage] = React.useState('');
+  const [avatarLink, setAvatarLink] = React.useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
     onUpdateAvatar({
-      avatar: inputRef.current.value,
+      avatar: avatarLink,
     });
   }
 
   React.useEffect(() => {
-    inputRef.current.value = '';
+    setAvatarLink('');
     updateUrlInputValidity(true);
   }, [isOpen]);
 
   return (
-    <>
-      <PopupWithForm
-        onSubmit={handleSubmit}
-        // isInvalid={!isUrlInputValid}
-        name="avatar"
-        title="Change Profile Picture"
-        buttonLabel={isSubmitting ? 'Saving...' : 'Save'}
-        isOpen={isOpen}
-        onClose={onClose}>
-        <div className="popup__input-container popup__input-container_role_avatar">
-          <input
-            ref={inputRef}
-            onChange={evt => checkValidity(evt, updateUrlInputValidity, updateErrorMessage)}
-            type="url"
-            id="avatar-url"
-            name="avatar"
-            placeholder="Image link"
-            className="popup__input popup__input_role_image-link"
-            required
-          />
-          <span className={`popup__input-error popup__input-error_avatar-url ${!isUrlInputValid && 'popup__input-error_active'}`}>
-            {errorMessage}
-          </span>
-        </div>
-      </PopupWithForm>
-    </>
+    <PopupWithForm
+      onSubmit={handleSubmit}
+      name="avatar"
+      title="Change Profile Picture"
+      buttonLabel={isSubmitting ? 'Saving...' : 'Save'}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="popup__input-container popup__input-container_role_avatar">
+        <input
+          onChange={(evt) => {
+            checkValidity(evt, updateUrlInputValidity, updateErrorMessage);
+            setAvatarLink(evt.target.value);
+          }}
+          type="url"
+          id="avatar-url"
+          name="avatar"
+          placeholder="Image link"
+          className="popup__input popup__input_role_image-link"
+          value={avatarLink}
+          required
+        />
+        <span
+          className={`popup__input-error popup__input-error_avatar-url ${
+            !isUrlInputValid && 'popup__input-error_active'
+          }`}
+        >
+          {errorMessage}
+        </span>
+      </div>
+    </PopupWithForm>
   );
 }
