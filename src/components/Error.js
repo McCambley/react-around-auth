dbd6c498e31899e0bf7ab3ee1295177e92c30155
McCambley <jakemccamb16@gmail.com>
2021-09-18 00:38:08 -0400

    Fix: update error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
   return (
     <section className="error">
-      <h1 className="error__header">Oops! Looks like the page you're looking for doesn't exist.</h1>
+      <h1 className="error__header">Oops!</h1>
+      <h2 className="error__paragraph">Looks like the page you're looking for doesn't exist.</h2>
+      <Link to="/" className="error__link">
+        Back home
+      </Link>
     </section>
   );
 }
