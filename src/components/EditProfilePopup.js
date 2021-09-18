2a8616640414dcaed619d85fb7491a5dce046d7c
McCambley <jakemccamb16@gmail.com>
2021-09-17 22:32:57 -0400

    Fix: remove fragments from editprofilepopup

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -2,7 +2,14 @@ import PopupWithForm from './PopupWithForm';
 import React from 'react';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
-export default function EditProfilePopup({ isOpen, onClose, onUpdateUser, isSubmitting, checkValidity, onOverlayClick }) {
+export default function EditProfilePopup({
+  isOpen,
+  onClose,
+  onUpdateUser,
+  isSubmitting,
+  checkValidity,
+  onOverlayClick,
+}) {
   const [name, updateName] = React.useState('');
   const [description, updateAbout] = React.useState('');
   const [isNameInputValid, updateNameInputValidity] = React.useState(true);
@@ -32,45 +39,58 @@ export default function EditProfilePopup({ isOpen, onClose, onUpdateUser, isSubm
   }, [currentUser, isOpen]);
 
   return (
-    <>
-      <PopupWithForm
-        onSubmit={handleSubmit}
-        name="edit"
-        title="Edit Profile"
-        buttonLabel={isSubmitting ? 'Saving...' : 'Save'}
-        isOpen={isOpen}
-        onClose={onClose}>
-        <div className="popup__input-container">
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Name"
-            value={name}
-            onChange={evt => handleChange(evt, updateName, updateNameInputValidity, updateNameErrorMessage)}
-            className="popup__input popup__input_role_name"
-            required
-            minLength="2"
-            maxLength="40"
-          />
-          <span className={`popup__input-error popup__input-error_name ${!isNameInputValid && 'popup__input-error_active'}`}>{nameErrorMessage}</span>
-          <input
-            type="text"
-            id="title"
-            name="about"
-            placeholder="Title"
-            value={description}
-            onChange={evt => handleChange(evt, updateAbout, updateAboutInputValidity, updateAboutErrorMessage)}
-            className="popup__input popup__input_role_title"
-            required
-            minLength="2"
-            maxLength="200"
-          />
-          <span className={`popup__input-error popup__input-error_title ${!isAboutInputValid && 'popup__input-error_active'}`}>
-            {aboutErrorMessage}
-          </span>
-        </div>
-      </PopupWithForm>
-    </>
+    <PopupWithForm
+      onSubmit={handleSubmit}
+      name="edit"
+      title="Edit Profile"
+      buttonLabel={isSubmitting ? 'Saving...' : 'Save'}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="popup__input-container">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(evt) =>
+            handleChange(evt, updateName, updateNameInputValidity, updateNameErrorMessage)
+          }
+          className="popup__input popup__input_role_name"
+          required
+          minLength="2"
+          maxLength="40"
+        />
+        <span
+          className={`popup__input-error popup__input-error_name ${
+            !isNameInputValid && 'popup__input-error_active'
+          }`}
+        >
+          {nameErrorMessage}
+        </span>
+        <input
+          type="text"
+          id="title"
+          name="about"
+          placeholder="Title"
+          value={description}
+          onChange={(evt) =>
+            handleChange(evt, updateAbout, updateAboutInputValidity, updateAboutErrorMessage)
+          }
+          className="popup__input popup__input_role_title"
+          required
+          minLength="2"
+          maxLength="200"
+        />
+        <span
+          className={`popup__input-error popup__input-error_title ${
+            !isAboutInputValid && 'popup__input-error_active'
+          }`}
+        >
+          {aboutErrorMessage}
+        </span>
+      </div>
+    </PopupWithForm>
   );
 }
